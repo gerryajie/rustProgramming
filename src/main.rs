8a5c388fb73e3b61da7042d0bfdecb360e950698
gerry <gerryajie@gmail.com>
2023-01-13 19:00:38 +0700

    rusty

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ use std::cmp::Ordering;
 // fn main(){
 //     const ONE_MIL:u32=1_000_000;
 //     const PI:f32=3.1415;
-//     let age:&str="47";
+//     let age:&str="36";
 //     let mut age:u32=age.trim().parse()
 //         .expect("Age wasnt assigned number");
 //     age=age+1;
@@ -50,15 +50,98 @@ use std::cmp::Ordering;
 //function ke 4
 
 
-fn main(){
-    let num_3:u32=5;
-    let num_4:u32=4;
+// fn main(){
+//     let num_3:u32=5;
+//     let num_4:u32=4;
 
-    println!("5 + 4 = {}",num_3+num_4);
-    println!("5 - 4 = {}",num_3-num_4);
-    println!("5 X 4 = {}",num_3*num_4);
-    println!("5 / 4 = {}",num_3/num_4);
-    println!("5 % 4 = {}",num_3%num_4);
+//     println!("5 + 4 = {}",num_3+num_4);
+//     println!("5 - 4 = {}",num_3-num_4);
+//     println!("5 X 4 = {}",num_3*num_4);
+//     println!("5 / 4 = {}",num_3/num_4);
+//     println!("5 % 4 = {}",num_3%num_4);
     
+// }
+//function ke 5 operasi  matematik
+
+// fn main(){
+//     let random_num=rand::thread_rng().gen_range(1..101);
+//     println!("Random:{}",random_num)
+// }
+
+//function cari angka random 1 sampai 100
+
+// fn main(){
+//     let age:i32=8;
+//     if (age>=1)&&(age<=18){
+//         println!("Important birthday");
+//     }else if (age==21)||(age==50) {
+//         println!("Important Birthday")        
+//     }else if age>=65 {
+//         println!("important Birthday")
+        
+//     }else {
+//         println!("Not an important birthday")
+//     }
+// }
+// function if
+
+// fn main(){
+//     let mut my_age:i32=47;
+//     let can_vote=if my_age>=18{
+//         true
+//     }else{
+//         false
+//     };
+//     println!("can vote:{}",can_vote)
+// }
+//functional conditional if
+
+// fn main(){
+//     let age2=30;
+//     match age2 {
+//         1..=18=>println!("important birtday"),
+//         21|50=>println!("important birthday"),
+//         65..=i32::MAX=>println!("important birthday"),
+//         _ =>println!("not an important birthday"),
+
+//     };
+// }
+
+//functional match conditional
+
+// fn main(){
+//     let my_age=20;
+//     let voting_age=18;
+//     match my_age.cmp(&voting_age){
+//         Ordering::Less=>println!("Anda Belum Bisa Memilih !"),
+//         Ordering::Greater=>println!("Anda Bisa Memilih"),
+//         Ordering::Equal=>println!("Waktu yang mantap untuk pilih"),
+//     };
+// }
+// functional match compare
+
+// fn main(){
+//     let arr_1=[1,2,3,4,5,6,7,8,9];
+//     println!("1st:{}",arr_1[0]);
+//     println!("2nd:{}",arr_1[1]);
+//     println!("lenght:{}",arr_1.len());
+
+// }
+// functional call the array
+
+fn main(){
+    let arr_2=[1,2,3,4,5,6,7,8,9];
+    let mut loop_idx=0;
+    loop{
+        if arr_2[loop_idx] %2 == 0{
+            loop_idx += 1;
+            continue;
+        }
+        if arr_2[loop_idx] == 9{
+            break;
+        }
+        println!("val : {}",arr_2[loop_idx]);
+        loop_idx += 1;
+    }
 }
-//function ke 5
+// function loop increment
