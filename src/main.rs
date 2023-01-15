7f2439974975361474852b97cf2065d35eec1880
gerry <gerryajie@gmail.com>
2023-01-15 14:57:05 +0700

    Rusty

--- src/main.rs
+++ src/main.rs
@@ -178,16 +178,40 @@ use std::cmp::Ordering;
 // tuple
 
 
-fn main(){
-    let mut st1=String::new();
-    st1.push('A');
-    st1.push_str("word");
-    for word in st1.split_whitespace(){
-        println!("{}",word);
-    }
-    let st2=st1.replace("A", "Another One"); 
-    println!("{}",st2)
+// fn main(){
+//     let mut st1=String::new();
+//     st1.push('A');
+//     st1.push_str("word");
+//     for word in st1.split_whitespace(){
+//         println!("{}",word);
+//     }
+//     let st2=st1.replace("A", "Another One"); 
+//     println!("{}",st2)
 
-}
+// }
 
 //function string replace whitespace
+
+fn main(){
+    let st3=String::from("x r t b h k k c a");
+    let mut v1:Vec<char>=st3.chars().collect();
+    v1.sort();
+    v1.dedup();
+    for char in v1{
+        println!("{}",char);
+    }
+    let st4:&str="Random String";
+    let mut st5:String=st4.to_string();
+    println!("{}",st5);
+    let byte_arr1=st5.as_bytes();
+    let st6=&st5[0..6];
+    println!("String Length : {}",st6.len());
+    st5.clear();
+    let st6=String::from("Just Some");
+    let st7=String::from("words");
+    let st8=st6+&st7;
+    for char in st8.bytes(){
+        println!("{}",char)
+    }
+}
+// function string 
