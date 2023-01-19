1b39577709c5d40fcc13dcff55411b2bdd1c2984
gerry <gerryajie@gmail.com>
2023-01-19 15:53:51 +0700

    rusty

--- src/main.rs
+++ src/main.rs
@@ -192,26 +192,35 @@ use std::cmp::Ordering;
 
 //function string replace whitespace
 
-fn main(){
-    let st3=String::from("x r t b h k k c a p s");
-    let mut v1:Vec<char>=st3.chars().collect();
-    v1.sort();
-    v1.dedup();
-    for char in v1{
-        println!("{}",char);
-    }
-    let st4:&str="Random String";
-    let mut st5:String=st4.to_string();
-    println!("{}",st5);
-    let byte_arr1=st5.as_bytes();
-    let st6=&st5[0..6];
-    println!("String Length : {}",st6.len());
-    st5.clear();
-    let st6=String::from("Just Some");
-    let st7=String::from("words");
-    let st8=st6+&st7;
-    for char in st8.bytes(){
-        println!("{}",char)
-    }
-}
+// fn main(){
+//     let st3=String::from("x r t b h k k c a p s");
+//     let mut v1:Vec<char>=st3.chars().collect();
+//     v1.sort();
+//     v1.dedup();
+//     for char in v1{
+//         println!("{}",char);
+//     }
+//     let st4:&str="Random String";
+//     let mut st5:String=st4.to_string();
+//     println!("{}",st5);
+//     let byte_arr1=st5.as_bytes();
+//     let st6=&st5[0..6];
+//     println!("String Length : {}",st6.len());
+//     st5.clear();
+//     let st6=String::from("Just Some");
+//     let st7=String::from("words");
+//     let st8=st6+&st7;
+//     for char in st8.bytes(){
+//         println!("{}",char)
+//     }
+// }
 // function string 
+
+fn main(){
+    let int_u8:u8=8;
+    let int2_u8:u8=5;
+    let int_u32:u32=(int_u8 as u32)+(int2_u8 as u32);
+    println!("Hasilnya : {}",int_u32)
+}
+
+
